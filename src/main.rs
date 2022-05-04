2ec615b8255bd926eb42498b336286bd3abcf1df
Tony Ketcham <tony@modyfi.io>
2022-05-03 21:08:50 -0400

    add set_element_properties helper

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use noise::{NoiseFn, Perlin};
 use svg::node::element::Element;
+use svg::node::Value;
 use svg::{Document, Node};
 fn main() {
     let mut document = Document::new().set("viewBox", (0, 0, 1000, 1000));
@@ -15,16 +16,18 @@ fn main() {
 
             let mut element = if f % 3. == 0. {
                 let mut rect = Element::new(String::from("rect"));
-                rect.assign("x", x);
-                rect.assign("y", y);
-                rect.assign("width", 15.);
-                rect.assign("height", f % 10.);
+
+                set_element_properties(
+                    &mut rect,
+                    vec![("x", x), ("y", y), ("width", 15.), ("height", f % 10.)],
+                );
+
                 rect
             } else {
                 let mut circle = Element::new(String::from("circle"));
-                circle.assign("cx", x);
-                circle.assign("cy", y);
-                circle.assign("r", f % 10.);
+
+                set_element_properties(&mut circle, vec![("cx", x), ("cy", y), ("r", f % 10.)]);
+
                 circle
             };
 
@@ -41,4 +44,15 @@ fn main() {
     }
 
     svg::save("image.svg", &document).unwrap();
+}
+
+/// Set many property attributes of an element, all at once.
+fn set_element_properties<T, U>(element: &mut Element, properties: Vec<(T, U)>)
+where
+    T: Into<String>,
+    U: Into<Value>,
+{
+    for (name, value) in properties {
+        element.assign(name, value);
+    }
 }
