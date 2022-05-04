111b6387da37eb0da693ba84d145fe1b3005d889
Tony Ketcham <tony@modyfi.io>
2022-05-03 20:43:01 -0400

    it's funky time

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,43 @@
 use noise::{NoiseFn, Perlin};
-use svg::node::element::Circle;
-use svg::Document;
-
+use svg::node::element::Element;
+use svg::{Document, Node};
 fn main() {
-    let mut document = Document::new().set("viewBox", (0, 0, 200, 900));
+    let mut document = Document::new().set("viewBox", (0, 0, 1000, 1000));
 
     let perlin = Perlin::new();
 
-    let circles = (0..400)
+    let nodes = (100..150)
         .map(|i| {
-            let f = i as f64;
-            let x = perlin.get([f * 0.02, f * 0.05, 0.]) * 700. + 5.;
+            let f = i as f64 * 4.0;
 
-            let y = 4000. / x;
-            Circle::new()
-                .set("cx", x)
-                .set("cy", 750. % y + 50.)
-                .set("r", 15. * perlin.get([x, y, 0.0]) + 5.)
-                .set("fill", "none")
-                .set("stroke", "black")
-                .set("stroke-width", 2)
+            let x = f64::abs(perlin.get([f % 3. * 0.02, f * 0.01, 0.]) * 1000.0) + 50.;
+            let y = f64::abs(perlin.get([f % 2. * 0.02, f * 0.005, 0.]) * 1000.) + 50.;
+
+            let mut element = if f % 3. == 0. {
+                let mut rect = Element::new(String::from("rect"));
+                rect.assign("x", x);
+                rect.assign("y", y);
+                rect.assign("width", 15.);
+                rect.assign("height", f % 10.);
+                rect
+            } else {
+                let mut circle = Element::new(String::from("circle"));
+                circle.assign("cx", x);
+                circle.assign("cy", y);
+                circle.assign("r", f % 10.);
+                circle
+            };
+
+            element.assign("fill", "none");
+            element.assign("stroke", "black");
+            element.assign("stroke-width", 2);
+
+            element
         })
         .collect::<Vec<_>>();
 
-    for circle in circles {
-        document = document.add(circle);
+    for node in nodes {
+        document = document.add(node);
     }
 
     svg::save("image.svg", &document).unwrap();
